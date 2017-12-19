5a36f0f700f01e4271562d22b7d3a2c4e1710972
brendonrjones <brendonrjones@gmail.com>
2017-12-19 14:41:20 +1100

    Update

---
+++ listcount.py
@@ -0,0 +1,17 @@
+with open('counter.csv', 'r') as array:
+    numbers=list()
+    N=0
+    ind=0
+    count=0
+    for val in array:
+        numbers.append(val)
+    numbers = list(map(int, numbers))
+    while ind<len(numbers):
+        N=numbers[ind]
+        if N>=3:
+            numbers[ind]=numbers[ind]-1
+        else:
+            numbers[ind]=numbers[ind]+1
+        ind=ind+N
+        count=count+1
+    print("Steps: ",count)
